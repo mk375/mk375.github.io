c26f3a84c97476c2e41eacc51bdca89f01c6d730
Martin Koch <Martin.koch00@gmx.de>
2025-08-16 08:12:45 +0200

    changed base root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-    base: '/mk375.github.io/',
+    base: '/',
     
   plugins: [
     tailwindcss(),
